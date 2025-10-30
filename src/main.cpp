91852c5fcab6da63d2286d204a139f77a8b53096
EvieUnderscore <evieunderscoreevil@gmail.com>
2025-10-30 11:06:14 -0400

    SDL3 Initialized and Blank Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,45 @@
 #include <iostream>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_video.h>
+#include <SDL3/SDL_main.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-	std::cout << "EVIL HELLO WORLDD HAHA" << std::endl;
+	// init everything here
+
+	SDL_Window* window;
+
+	SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_EVENTS);
+	window = SDL_CreateWindow(
+		"20 Games Challenge | Pong", 
+		1280, 960, 
+		NULL
+	);
+	if (window == NULL) {
+		SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Could not create window: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	// main game loop
+
+	bool running = true;
+	while (running) {
+		SDL_Event event;
+		while (SDL_PollEvent(&event)) {
+			// polled event behavior
+
+			if (event.type == SDL_EVENT_QUIT) {
+				running = false;
+			}
+		}
+
+		
+	}
+
+	// uninit everything here
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
 	return 0;
 }
